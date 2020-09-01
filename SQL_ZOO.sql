2834a8eab02958034bdb9b32596a7a8385c51afb
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-09-01 20:20:35 +0300

    ADD problem 2 answers

--- SQL_ZOO.sql
+++ SQL_ZOO.sql
@@ -94,32 +94,94 @@ SELECT name, continent, population FROM world
 SELECT name FROM world
 WHERE population >= 200000000
 
+-- 3
+SELECT name, SUM(gdp/population) AS per_capita_GDP FROM world
+WHERE population >= 200000000
+GROUP BY name
+
+-- 4
+SELECT name, SUM(population / 1000000) pop_in_mil FROM world
+WHERE continent = 'South America'
+GROUP BY name
+
+-- 5
+SELECT name, population FROM world
+WHERE name IN ('France','Germany', 'Italy')
+
+-- 6
+SELECT name FROM world
+WHERE name LIKE '%united%'
+
+-- 7
+SELECT name, population, area FROM world
+WHERE area > 3000000 OR population > 250000000  
+
+-- 8
+SELECT name, population, area FROM world
+WHERE area > 3000000 XOR population > 250000000  
+
+-- 9
+SELECT name, ROUND(SUM(population/1000000), 2) AS population_mil, ROUND(SUM(gdp/1000000000), 2) AS gdp_bil FROM world
+WHERE continent = 'South America'
+GROUP BY name
+
+-- 10
+SELECT name, ROUND(SUM(gdp/population), -3) AS per_capita FROM world
+WHERE gdp > 1000000000000
+GROUP BY name
+
+-- 11
+SELECT name, capital FROM world
+WHERE LENGTH(name) = LENGTH(capital)
+
+-- 12
+SELECT name, capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital
+
+-- 13
+SELECT name
+FROM world
+WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %'
+
+
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
 -- Problem 3
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 4
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 5
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 6
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 7
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 8
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 8+
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 9-
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 9+
 
+-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 -- Problem 9
 
