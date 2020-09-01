59b8307e5e340475e9b98342629dacc511538f08
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-09-01 13:50:42 +0300

    ADD problem 1 solution

---
+++ SQL_ZOO.sql
@@ -0,0 +1,37 @@
+-- Problem 1
+SELECT population FROM world
+WHERE name = 'Germany'
+
+
+-- Problem 2
+
+
+-- Problem 3
+
+
+-- Problem 4
+
+
+-- Problem 5
+
+
+-- Problem 6
+
+
+-- Problem 7
+
+
+-- Problem 8
+
+
+-- Problem 8+
+
+
+-- Problem 9-
+
+
+-- Problem 9+
+
+
+-- Problem 9
+
